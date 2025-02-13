0756ff8ec47d82cbe253d9e5b13868b1ee1caafc
Ritzz09 <seexpert111.com>
2025-02-14 01:44:43 +0530

    last

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,6 +3,7 @@ import logo from "./assets/faq-accordion-card-main/images/header.png";
 import "../components/header.css";
 import { color } from "framer-motion";
 import { RxFontStyle } from "react-icons/rx";
+import { Button } from "@material-tailwind/react";
 
 export const Header = (props) => {
   return (
@@ -12,26 +13,30 @@ export const Header = (props) => {
           <div className="container">
             <div className="row align-items-center"> {/* Ensures vertical alignment */}
               {/* Left Section - Text */}
-              <div className="col-md-6 intro-text">
+              <div className="col-md-8 intro-text">
                 <h1>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
                 <p1 style={{color:"white",fontWeight:'2000', fontSize:'22px'}}>{props.data ? props.data.paragraph : "Loading"}</p1>
-                
+                <span></span>
+                <div >
+                <a style={{fontWeight:'bold',fontSize:"20px"}} href="#contact" className="btn btn-custom  page-scroll" >
+                    Enquire Now
+                </a>
+                </div>
                 
               </div>
+              
               {/* Right Section - Image */}
-              <div className="col-md-6 text-center">
+              <div className="col-md-4 text-center">
                 <img 
                   src={logo}  // Update with your image path
                   alt="Right Section Image"
                   className="img-fluid header-image" 
                 />
               </div>
-                <a style={{fontWeight:'bold',fontSize:"20px"}} href="#contact" className="btn btn-custom  page-scroll">
-                    Enquire Now
-                </a>
+               
             </div>
           </div>
         </div>
