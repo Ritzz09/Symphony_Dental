692de7b7c43744291181080d356f674c3fb0f105
Ritzz09 <seexpert111@gmail.com>
2025-08-28 17:28:54 +0530

    seo

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const SEO = () => {
   return (
     <>
       {/* Local Business Schema */}
       <script type="application/ld+json">
+       <Helmet>
+      <title>Dentist in Khar West | Dental Clinic in Khar/Bandra</title>
+      <meta
+        name="description"
+        content="Looking for a dentist in Khar/Bandra? Symphony Dental Care offers expert dental services with a focus on patient comfort, care, and the latest treatments for all your oral health needs."
+      />
+      <link rel="canonical" href="https://www.symphonydentalcare.in/" />
+    </Helmet>
+
         {JSON.stringify({
           "@context": "https://schema.org",
           "@type": "LocalBusiness",
